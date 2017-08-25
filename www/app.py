a8fa30c828aeda35fe142955e4796ab0a92ee135
w l <w l>
2017-08-25 14:12:23 +0800

    push codes

--- www/app.py
+++ www/app.py
@@ -132,7 +132,7 @@ async def init(loop):
     init_jinja2(app, filters=dict(datetime=datetime_filter))
     add_routes(app, 'handlers')
     add_static(app)
-    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 80)
+    srv = await loop.create_server(app.make_handler(), '0.0.0.0', 80)
     logging.info('server started at http://localhost:80...')
     return srv
 
